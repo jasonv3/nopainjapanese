55d6e48d651b5e2ba22a105c8337b1ce7e96804e
“jasonv3” <jason5231192@126.com>
2020-08-04 18:52:11 +0800

    print

--- back-end/config.py
+++ back-end/config.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+print(os.path.join(basedir, '.env'))
 load_dotenv(os.path.join(basedir, '.env'))
 
 
